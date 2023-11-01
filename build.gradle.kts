c4d137a4b63707e8e4c910ae7735385171172a1d
Ryder Belserion <no-reply@ryderbelserion.com>
2023-11-01 14:22:41 -0400

    bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
 
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.20"
 
     application
 }
@@ -21,10 +21,10 @@ dependencies {
 
     implementation("com.google.code.gson", "gson", "2.10.1")
 
-    implementation("net.dv8tion", "JDA", "5.0.0-beta.12")
-    implementation("ch.qos.logback", "logback-classic", "1.4.5")
+    implementation("net.dv8tion", "JDA", "5.0.0-beta.17")
+    implementation("ch.qos.logback", "logback-classic", "1.4.11")
 
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.6.4")
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.7.3")
 }
 
 kotlin {
