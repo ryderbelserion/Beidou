9fdb2f1ca7629de4ab697748409406977616c345
Ryder Belserion <no-reply@ryderbelserion.com>
2023-12-04 12:10:54 -0500

    ok

---
+++ src/main/kotlin/com/ryderbelserion/bot/listeners/MsgListener.kt
@@ -0,0 +1,29 @@
+package com.ryderbelserion.bot.listeners
+
+import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.events.message.MessageDeleteEvent
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+
+class MsgListener : ListenerAdapter() {
+
+    private val messages = hashMapOf<Long, Message>()
+
+    override fun onMessageReceived(event: MessageReceivedEvent) {
+        if (event.author.isBot || event.isWebhookMessage) return
+
+        val author = event.author
+
+        val id = event.messageIdLong
+
+        messages[id] = event.message
+    }
+
+    override fun onMessageDelete(event: MessageDeleteEvent) {
+        val channel = event.channel.asTextChannel()
+
+        val data = messages[event.messageIdLong]
+
+        event.channel
+    }
+}
