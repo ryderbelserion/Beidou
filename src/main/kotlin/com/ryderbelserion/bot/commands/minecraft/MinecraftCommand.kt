64cb33a9ecd2fee77a60b6187a5e6767440dcc9b
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-18 16:35:30 -0400

    Add thumbnail and failure embed

--- src/main/kotlin/com/ryderbelserion/bot/commands/minecraft/MinecraftCommand.kt
+++ src/main/kotlin/com/ryderbelserion/bot/commands/minecraft/MinecraftCommand.kt
@@ -4,7 +4,6 @@ import com.ryderbelserion.api.commands.CommandContext
 import com.ryderbelserion.api.commands.CommandEngine
 import com.ryderbelserion.api.embeds.Embed
 import com.ryderbelserion.api.embeds.EmbedColors
-import com.ryderbelserion.api.util.StringUtil
 import com.ryderbelserion.bot.Beidou
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -17,8 +16,6 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import net.dv8tion.jda.api.Permission
-import java.nio.file.Files
-import java.nio.file.Path
 
 public class MinecraftCommand(private val plugin: Beidou) : CommandEngine("minecraft", "A set of commands related to Minecraft", Permission.MESSAGE_SEND) {
 
@@ -31,26 +28,39 @@ public class MinecraftCommand(private val plugin: Beidou) : CommandEngine("minec
             return
         }
 
-        var server: Server
+        runCatching {
+            val server: Server
 
-        runBlocking {
-            server = MinecraftServer(ip).getServer()
-        }
-
-        val color = if (server.isOnline) EmbedColors.SUCCESS else EmbedColors.FAIL
-
-        val image = StringUtil.convertToImage(server.icon.split(",")[1], this.plugin.getGuildDirectory(context.guild()?.id)).path
-
-        val embed = Embed()
-            .author(context.author())
-            .fields {
-                field("Server IP", ip, true)
-                field("Online/Max", "${server.players.playerCount}/${server.players.maxCount}", true)
+            runBlocking {
+                server = MinecraftServer(ip).getServer()
             }
-            .thumbnail("attachment://$image")
-            .color(color)
 
-        context.reply(embed.build(), false)
+            val color = if (server.isOnline) EmbedColors.SUCCESS else EmbedColors.FAIL
+
+            val embed = Embed()
+                .author(context.author())
+                .fields {
+                    field("Server IP", ip, true)
+                }
+                .thumbnail("https://api.mcsrvstat.us/icon/$ip")
+                .color(color)
+
+            if (server.isOnline) {
+                embed.fields {
+                    field("Server Version", server.protocol.version, true)
+                    field("Online/Max", "${server.players.playerCount}/${server.players.maxCount}", true)
+                }
+            }
+
+            context.reply(embed.build(), false)
+        }.onFailure {
+            val embed = Embed()
+                .author(context.author())
+                .description("The server you entered is not online.")
+                .color(EmbedColors.FAIL)
+
+            context.reply(embed.build(), false)
+        }
     }
 }
 
@@ -68,14 +78,15 @@ public class MinecraftServer(ip: String) {
     }
 
     public suspend fun getServer(): Server {
-        val response: Server = this.client.get(this.url).body()
-
-        return response
+        return this.client.get(this.url).body<Server>()
     }
 }
 
 @Serializable
-public data class Server(@SerialName("online") val isOnline: Boolean, val players: Players, val icon: String)
+public data class Server(@SerialName("online") val isOnline: Boolean, val players: Players, val protocol: Protocol, val icon: String, val code: String = "")
 
 @Serializable
 public data class Players(@SerialName("online") val playerCount: String, @SerialName("max") val maxCount: String)
+
+@Serializable
+public data class Protocol(@SerialName("name") val version: String)
