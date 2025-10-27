f592ad7436bbb0d3eac8cc03a5c918dda59e7d6a
Ryder Belserion <contact@ryderbelserion.com>
2025-10-27 00:18:35 -0400

    Add gateway intents

--- src/main/java/com/ryderbelserion/discord/bot/Beidou.java
+++ src/main/java/com/ryderbelserion/discord/bot/Beidou.java
@@ -3,6 +3,8 @@ package com.ryderbelserion.discord.bot;
 import com.ryderbelserion.discord.api.DiscordPlugin;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import java.util.List;
@@ -10,7 +12,28 @@ import java.util.List;
 public class Beidou extends DiscordPlugin {
 
     public Beidou(@NotNull final String token, @NotNull final Logger logger) {
-        super(List.of(), List.of(), "Beidou", logger, token);
+        super(List.of(
+                // direct messages
+                GatewayIntent.DIRECT_MESSAGE_REACTIONS,
+                GatewayIntent.DIRECT_MESSAGE_TYPING,
+                GatewayIntent.DIRECT_MESSAGES,
+
+                // universal
+                GatewayIntent.MESSAGE_CONTENT,
+
+                // other
+                GatewayIntent.SCHEDULED_EVENTS,
+
+                // guilds
+                GatewayIntent.GUILD_VOICE_STATES,
+                GatewayIntent.GUILD_EXPRESSIONS,
+                GatewayIntent.GUILD_PRESENCES,
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.GUILD_WEBHOOKS
+        ), List.of(
+                CacheFlag.VOICE_STATE,
+                CacheFlag.EMOJI
+        ), "beidou", logger, token);
     }
 
     @Override
