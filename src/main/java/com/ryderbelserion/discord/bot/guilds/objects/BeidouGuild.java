10e3607de6dc93dd3aea8f4a14a4cf12c8e9769e
Ryder Belserion <contact@ryderbelserion.com>
2025-10-30 23:17:51 -0400

    Save guild name to cache on startup, so I know which guild I am looking at

--- src/main/java/com/ryderbelserion/discord/bot/guilds/objects/BeidouGuild.java
+++ src/main/java/com/ryderbelserion/discord/bot/guilds/objects/BeidouGuild.java
@@ -2,22 +2,38 @@ package com.ryderbelserion.discord.bot.guilds.objects;
 
 import com.ryderbelserion.discord.bot.guilds.GuildConfig;
 import com.ryderbelserion.fusion.files.FileManager;
+import net.dv8tion.jda.api.entities.Guild;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
+import org.spongepowered.configurate.BasicConfigurationNode;
 import org.spongepowered.configurate.CommentedConfigurationNode;
+import org.spongepowered.configurate.serialize.SerializationException;
 import java.nio.file.Path;
 
 public class BeidouGuild {
 
     private CommentedConfigurationNode config;
+
     private GuildConfig guildConfig;
 
-    public BeidouGuild(@NotNull final FileManager fileManager, @NotNull final Logger logger, @NotNull final Path directory) {
-        init(fileManager, logger, directory);
+    public BeidouGuild(@NotNull final FileManager fileManager, @NotNull final Logger logger, @NotNull final Path directory, @NotNull final Guild guild) {
+        init(fileManager, logger, directory, guild);
     }
 
-    public void init(@NotNull final FileManager fileManager, @NotNull final Logger logger, @NotNull final Path directory) {
+    public void init(@NotNull final FileManager fileManager, @NotNull final Logger logger, @NotNull final Path directory, @NotNull final Guild guild) {
         fileManager.getYamlFile(directory.resolve("config.yml")).ifPresent(customFile -> this.config = customFile.getConfiguration());
+
+        fileManager.getJsonFile(directory.resolve("cache.json")).ifPresent(customFile -> {
+            final BasicConfigurationNode cache = customFile.getConfiguration();
+
+            try {
+                cache.node("name").set(String.class, guild.getName());
+
+                customFile.save();
+            } catch (final SerializationException exception) {
+                exception.printStackTrace();
+            }
+        });
 
         if (this.config != null) {
             this.guildConfig = new GuildConfig(this.config, logger);
